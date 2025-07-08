0ab85266cc6176332e3ba065e4507c960f94c30e
Moises <moisesdavidlara1@gmail.com>
2025-07-08 14:15:34 -0400

    Corregir error tipográfico en la importación de Google y actualizar la configuración de cliente en auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,18 @@
 // auth.config.ts
-import Goggle from '@auth/core/providers/google'
+import Google from '@auth/core/providers/google'
 import { defineConfig } from 'auth-astro'
+import { config } from '@/config'
+
+
+
+
 
 export default defineConfig({
 	providers: [
-		Goggle({
-			clientId: import.meta.env.GOGGLE_CLIENT_ID,
-			clientSecret: import.meta.env.GOGGLE_CLIENT_SECRET,
+		Google({
+			clientId: config.env.GOGGLE_CLIENT_ID,
+			clientSecret: config.env.GOGGLE_CLIENT_SECRET,
 		}),
 	],
 })
+
