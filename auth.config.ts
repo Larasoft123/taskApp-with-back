2e18a9e0c176877bb15c2eda2518c96c9edbb827
Moises <moisesdavidlara1@gmail.com>
2025-07-09 13:42:35 -0400

    Refactor configuración de autenticación para incluir adaptador Prisma y mejorar manejo de sesiones

--- auth.config.ts
+++ auth.config.ts
@@ -1,18 +1,56 @@
 // auth.config.ts
-import Google from '@auth/core/providers/google'
-import { defineConfig } from 'auth-astro'
-import { config } from '@/config'
-
-
-
-
+import Google from "@auth/core/providers/google";
+import { defineConfig } from "auth-astro";
+import { config } from "@/config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db";
 
 export default defineConfig({
-	providers: [
-		Google({
-			clientId: config.env.GOGGLE_CLIENT_ID,
-			clientSecret: config.env.GOGGLE_CLIENT_SECRET,
-		}),
-	],
-})
+  adapter: PrismaAdapter(db),
 
+  secret: config.env.AUTH_SECRET,
+  providers: [
+    Google({
+      clientId: config.env.GOGGLE_CLIENT_ID,
+      clientSecret: config.env.GOGGLE_CLIENT_SECRET,
+    }),
+  ],
+
+  cookies: {},
+
+  session: {
+    strategy: "jwt",
+    },
+  pages: {
+    signIn: "/auth",
+    error: "/auth/error",
+  },
+  theme: {
+    colorScheme: "dark",
+  },
+  callbacks: {
+	
+    jwt: async (params) => {
+		console.log("jwt ",params)
+		
+		return {}
+
+	},
+
+	signIn: async (params) => {
+		console.log("sigin ",params)
+		return true
+	},
+
+	session: async (params) => {
+		console.log("session ",params)
+
+
+		return params.session
+	}
+
+
+  },
+
+  events: {},
+});
