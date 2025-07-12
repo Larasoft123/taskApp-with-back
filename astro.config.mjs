4a4c947b3b629faf89f22381821094c1d91d9c16
Moises <moisesdavidlara1@gmail.com>
2025-07-12 18:23:00 -0400

    add astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,35 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import auth from 'auth-astro';
+import auth from "auth-astro";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
-
-import preact from '@astrojs/preact';
-
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   vite: {
-    plugins: [tailwindcss()]
+    ssr: {
+      noExternal: ["react-hook-form"],
+    },
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat", // Must be below test-utils
+        "react/jsx-runtime": "preact/jsx-runtime",
+      },
+    },
   },
 
-  integrations: [auth(), preact()],
+  integrations: [auth(), preact({ compat: true, include: ["**/preact/*"] })],
 
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: "standalone",
+  }),
 });
