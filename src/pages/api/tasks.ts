2caac5a366dc79505d304af1cdc7bc0f92504977
Moises <moisesdavidlara1@gmail.com>
2025-07-12 18:24:50 -0400

    add api for create and read

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -1,21 +1,65 @@
 import { type APIRoute } from "astro";
 import { db } from "@/lib/db";
+import { getUserSession } from "@/utils/auth/getSession";
 
-export const GET: APIRoute = async ({params,request}) => {
-    console.log(params);
-    console.log(request);
+function getUrlParams(url: string) {
+  const pattern = "[&|?]([a-zA-Z0-9._%-]+)=([a-zA-Z0-9._%-]+)";
 
-    const result = await db.tasks.findMany()
+  const answer = Array.from(url.matchAll(pattern as any));
 
-     console.log(result);
+  return answer.map((match) => {
+    const [complete, key, value] = match;
+    return { complete, key, value };
+  });
+}
 
 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const GET: APIRoute = async ({ request }) => {
+  const params = getUrlParams(request.url);
+  const userId = params.find((param) => param.key === "userId")?.value;
+
+  if (!userId) return new Response("userId is required", { status: 400 });
+
+  
+
+  const result = await db.tasks.findMany({
+    where: {
+      userId,
+    },
+  });
 
 
+  await delay(5000);
 
 
   return new Response(JSON.stringify(result));
 };
 
+export const POST: APIRoute = async ({ request }) => {
+  const body = await request.json();
+  const { title, description, status, type } = body;
 
+  const { userId } = await getUserSession(request);
+  if (!userId) return new Response("userId is required", { status: 400 });
 
+  const data = {
+    userId,
+    title,
+    description,
+    status,
+    type,
+  };
+
+  try {
+    const result = await db.tasks.create({
+      data,
+    });
+    console.log({ result });
+    return new Response(JSON.stringify(result), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
+};
