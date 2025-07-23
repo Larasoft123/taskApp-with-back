85e8116d0c195bf47324aed437af25fca1608334
Moises <moisesdavidlara1@gmail.com>
2025-07-23 10:58:02 -0400

    change names of goggle env variables

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ export const config = {
     env: {
         TURSO_DATABASE_URL: import.meta.env.TURSO_DATABASE_URL,
         TURSO_AUTH_TOKEN: import.meta.env.TURSO_AUTH_TOKEN,
-        GOGGLE_CLIENT_ID: import.meta.env.GOOGLE_CLIENT_ID,
-        GOGGLE_CLIENT_SECRET: import.meta.env.GOOGLE_CLIENT_SECRET,
+        GOOGLE_CLIENT_ID: import.meta.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: import.meta.env.GOOGLE_CLIENT_SECRET,
         AUTH_SECRET: import.meta.env.AUTH_SECRET
 
 
