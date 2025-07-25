32538a6c0f83fc22aeb660569f8a26b31e5a764f
Moises <moisesdavidlara1@gmail.com>
2025-07-25 15:32:27 -0400

    refactor: simplifica la obtención de userId y mejora la gestión de etiquetas en la creación de tareas

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -2,27 +2,14 @@ import { type APIRoute } from "astro";
 import { db } from "@/lib/db";
 import { getUserSession } from "@/utils/auth/getSession";
 
-function getUrlParams(url: string) {
-  const pattern = "[&|?]([a-zA-Z0-9._%-]+)=([a-zA-Z0-9._%-]+)";
-
-  const answer = Array.from(url.matchAll(pattern as any));
-
-  return answer.map((match) => {
-    const [complete, key, value] = match;
-    return { complete, key, value };
-  });
-}
-
-
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const GET: APIRoute = async ({ request }) => {
-  const params = getUrlParams(request.url);
-  const userId = params.find((param) => param.key === "userId")?.value;
+  const url = new URL(request.url);
+  const searchParams = url.searchParams;
+  const userId = searchParams.get("userId");
 
   if (!userId) return new Response("userId is required", { status: 400 });
-
-  
 
   const result = await db.tasks.findMany({
     where: {
@@ -30,16 +17,14 @@ export const GET: APIRoute = async ({ request }) => {
     },
   });
 
-
-  await delay(5000);
-
+  // await delay(50);
 
   return new Response(JSON.stringify(result));
 };
 
 export const POST: APIRoute = async ({ request }) => {
   const body = await request.json();
-  const { title, description, status, type } = body;
+  const { title, description, status, type, tags } = body;
 
   const { userId } = await getUserSession(request);
   if (!userId) return new Response("userId is required", { status: 400 });
@@ -50,14 +35,20 @@ export const POST: APIRoute = async ({ request }) => {
     description,
     status,
     type,
+    tags: { connect: tags.map((tagId: number) => ({ id: tagId })) },
   };
 
   try {
+    // crear tarea y relacionarla con los id de etiqueta pasados
     const result = await db.tasks.create({
-      data,
+      data
     });
-    console.log({ result });
+
+
+
     return new Response(JSON.stringify(result), { status: 200 });
+
+
   } catch (error) {
     console.log(error);
     return new Response(JSON.stringify(error), { status: 500 });
