27b7d6b111ac6031fa85213727a8b45538669098
Moises <moisesdavidlara1@gmail.com>
2025-08-02 17:46:38 -0400

    change the default open to false

--- src/stores/sidenav-store.ts
+++ src/stores/sidenav-store.ts
@@ -1,6 +1,6 @@
 import { atom } from "nanostores";
 
-export const isSidenavOpen = atom(true);
+export const isSidenavOpen = atom(false);
 
 export const closeSidenav = () => {
   isSidenavOpen.set(false);
