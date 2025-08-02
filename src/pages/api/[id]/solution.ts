50e5560868a6afdf849ba38c19327f18f1e1a78b
Moises <moisesdavidlara1@gmail.com>
2025-08-02 17:47:03 -0400

    add endpoint for delete solutions

---
+++ src/pages/api/[id]/solution.ts
@@ -0,0 +1,30 @@
+import { type APIRoute } from "astro";
+import { db } from "@/lib/db";
+import { getUserSession } from "@/utils/auth/getSession";
+
+export const DELETE: APIRoute = async ({ params, request }) => {
+  const id = Number(params.id);
+
+  if (!id) return new Response("id is required", { status: 400 });
+
+  const { userId } = await getUserSession(request);
+
+  if (!userId) return new Response("userId is required", { status: 400 });
+
+  try {
+    const result = await db.solutions.delete({
+      where: {
+        id,
+        note: {
+            userId: userId
+        }
+      },
+    });
+
+    console.log(result)
+    return new Response(JSON.stringify(result), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
+};
