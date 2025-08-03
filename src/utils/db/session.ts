768b4c40cf1523d087210c9e9e677107229ac080
Moises <moisesdavidlara1@gmail.com>
2025-08-03 18:47:08 -0400

    add try cath

--- src/utils/db/session.ts
+++ src/utils/db/session.ts
@@ -2,26 +2,32 @@ import { getSession } from "auth-astro/server";
 
 export class Session {
   static async getUserId(request: Request) {
-    const session = await getSession(request);
+    try {
+      const session = await getSession(request);
+   
+      
 
-    if (!session) {
-      return new Response("Unauthorized", {
-        status: 401,
+    
+      if (!session) {
+        return new Response("Unauthorized", {
+          status: 401,
+        });
+      }
+
+      const { user } = session;
+      const id = user?.id;
+
+      if (!user || !id) {
+        return new Response("Unauthorized", {
+          status: 401,
+        });
+      }
+
+      return id;
+    } catch (error) {
+      return new Response("Internal Server Error", {
+        status: 500,
       });
     }
-
-    const { user } = session;
-    const id = user?.id;
-
-    if (!user || !id) {
-      return new Response("Unauthorized", {
-        status: 401,
-      });
-    }
-
-    return id;
   }
-
-
-
 }
